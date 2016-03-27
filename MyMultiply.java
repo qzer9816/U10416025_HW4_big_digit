db45df0c29d5c79ca7c36c635cfebc6431d0ab2c
qzer9816 <qzer9816@gmail.com>
2016-03-27 16:38:12 +0800

    乘數高低位對調

--- MyMultiply.java
+++ MyMultiply.java
@@ -10,11 +10,29 @@ public class MyMultiply {
 		System.out.println("Enter the second number:");
 		String str2 = console.readLine();
 		
-		//數字長度
+	//數字長度
         int len1 = str1.length();
         int len2 = str2.length();
 		
-		//陣列
+	//陣列
         char[] s1 = str1.toCharArray();
         char[] s2 = str2.toCharArray();
+        
+        //高低位對調
+        covertdata(s1, len1);  
+        covertdata(s2, len2);  
+        System.out.println("乘數:"+str1);  
+        System.out.println("乘數:"+str2);
+
+        multiply(s1, len1, s2, len2);
+    }
+    
+    public static void covertdata(char data[], int len) {
+        //高低位對調
+        for (int i = 0; i < len / 2; i++) {
+            data[i] += data[len - 1 - i];
+            data[len - 1 - i] = (char) (data[i] - data[len - 1 - i]);
+            data[i] = (char) (data[i] - data[len - 1 - i]);
+        }
+    }
 }

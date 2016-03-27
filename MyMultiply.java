3feeacf375ca4b6cf5a2498adc66797637722f94
qzer9816 <qzer9816@gmail.com>
2016-03-27 17:02:28 +0800

    排版
    將註解改成英文並排版

--- MyMultiply.java
+++ MyMultiply.java
@@ -1,34 +1,34 @@
-//U10416025 張詠淳
+//U10416025
 import java.io.Console;
 public class MyMultiply {
     //main method
     public static void main(String[] args) {
-		//藉由System新增的console( )方法取得標準輸入輸出裝置的Console物件str1,str2
-		Console console = System.console();
-		System.out.println("Enter the first number:");
-		String str1 = console.readLine();
-		System.out.println("Enter the second number:");
-		String str2 = console.readLine();
+	//use console() method get the str1 and str2
+	Console console = System.console();
+	System.out.println("Enter the first number:");
+	String str1 = console.readLine();
+	System.out.println("Enter the second number:");
+	String str2 = console.readLine();
 		
-	//數字長度
+	//int str1 and str2 length
         int len1 = str1.length();
         int len2 = str2.length();
 		
-	//陣列
+	//array
         char[] s1 = str1.toCharArray();
         char[] s2 = str2.toCharArray();
         
-        //高低位對調
+        //swap the level of digital bits
         covertdata(s1, len1);  
         covertdata(s2, len2);  
-        System.out.println("乘數:"+str1);  
-        System.out.println("乘數:"+str2);
+        System.out.println("number1:"+str1);  
+        System.out.println("number2:"+str2);
 
         multiply(s1, len1, s2, len2);
     }
     
     public static void covertdata(char data[], int len) {
-        //高低位對調
+        //swap the level of digital bits
         for (int i = 0; i < len / 2; i++) {
             data[i] += data[len - 1 - i];
             data[len - 1 - i] = (char) (data[i] - data[len - 1 - i]);
@@ -37,37 +37,37 @@ public class MyMultiply {
     }
     
      public static void multiply(char a[], int alen, char b[], int blen) {  
-        //兩數乘積位數不會超過成數位數和+3位
+        //cause the answer can not exceed the two numbers digital total
         int csize = alen + blen + 3;  
-	//定義乘積數陣列
+	//int the answer array
         int[] c = new int[csize];  
-        //陣列填充0
+        //add 0 in the answer array
         for (int ii = 0; ii < csize; ii++) {  
             c[ii] = 0;  
         }  
-        //對齊後逐位相乘
+        //multiply the two numbers
         for (int j = 0; j < blen; j++) {  
             for (int i = 0; i < alen; i++) {  
                 c[i + j] +=  Integer.parseInt(String.valueOf(a[i]))* Integer.parseInt(String.valueOf(b[j]));  
             }  
         }  
         int m = 0;  
-        //進位處理
+        //deal with the carry number
         for (m = 0; m < csize; m++) {  
             int carry = c[m] / 10;  
             c[m] = c[m] % 10;  
             if (carry > 0)  
                 c[m + 1] += carry;  
         }  
-        //找到最高位
+        //find the head number of answer
         for (m = csize - 1; m >= 0;) {  
             if (c[m] > 0)  
                 break;  
             m--;  
         }
         
-        //由最高位開始印出乘積
-        System.out.print("乘積:");
+        //print the answer
+        System.out.print("multiply answer:");
         for (int n = 0; n <= m; n++) {
             System.out.print(c[m - n]);
         }
